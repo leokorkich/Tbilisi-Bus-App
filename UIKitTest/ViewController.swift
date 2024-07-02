3ae63adcf4080d462f14e9127cbb526217a257eb
le0ne <leqorqia@gmail.com>
2024-07-02 18:23:56 +0400

    Initial Commit

---
+++ UIKitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTest
+//
+//  Created by Levan Qorqia on 02.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
